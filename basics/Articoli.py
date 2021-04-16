4ba3865f521ef216ef7206e11bd23a8afbe2b713
loviuz <loviuz@gmail.com>
2021-04-16 10:10:35 +0200

    Completamento inserimento/modifica articoli

--- basics/Articoli.py
+++ basics/Articoli.py
@@ -2,7 +2,6 @@ from common.Test import Test, get_html
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-
 class Articoli(Test):
     def setUp(self):
         super().setUp()
@@ -15,7 +14,9 @@ class Articoli(Test):
         self.creazione_articolo("01", "Articolo")
 
     def creazione_articolo(self, codice: str, descrizione: str):
-        ''' Crea un nuovo articolo. '''
+        ''' '''
+        ''' 1/2 Crea un nuovo articolo. '''
+        ''' '''
         # Apre la schermata di nuovo elemento
         self.find(By.CSS_SELECTOR, '#tabs > li:first-child .btn-primary > .fa-plus').click()
         modal = self.wait_modal()
@@ -26,17 +27,19 @@ class Articoli(Test):
         # Submit
         modal.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
         self.wait_loader()
-        
-        #toast = self.driver.find_elements(By.CLASS_NAME, 'toast-message')
-        #self.assertIn('Aggiunto articolo', toast)
+       
 
-        check = self.input(self.find(By.XPATH, '//input[@id="qta_manuale"]'), 'Modifica quantità')
-        self.driver.execute_script("arguments[0].click();", check)
 
-        self.input(By.XPATH, '//input[@id ="qta"]').setValue(qta)
+        ''' '''
+        ''' 2/2 Modifica articolo. '''
+        ''' '''
+        self.input(self.find(By.XPATH, '//div[@id="tab_0"]'), 'Modifica quantità').clickFlag()
 
-        self.find(By.XPATH, '//a[@id ="save"]').click()
+        ''' Q.tà '''
+        self.input(self.find(By.XPATH, '//div[@id="tab_0"]'), 'Quantità').setValue("1")
 
-    
-        
-        
+        ''' Causale movimento '''
+        self.input(self.find(By.XPATH, '//div[@id="tab_0"]'), 'Descrizione movimento').setValue("Movimento di test")
+
+        ''' Salvataggio '''
+        self.find(By.XPATH, '//div[@id="tab_0"]//a[@id="save"]').click()
