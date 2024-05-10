2213324a3934a0b46bf180cc03b5f12d168b09c3
Pek5892 <pek5892@gmail.com>
2024-05-10 14:44:46 +0200

    Aggiunta test Tags

---
+++ tests/30_Tags.py
@@ -0,0 +1,100 @@
+from common.Test import Test, get_html
+from selenium.webdriver.common.keys import Keys
+from common.RowManager import RowManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Tags(Test):
+    def setUp(self):
+        super().setUp()
+
+        self.expandSidebar("Attività")
+        
+    def test_creazione_tags(self):
+        # Creazione tags 
+        self.creazione_tags("Tags di Prova da Modificare")
+        self.creazione_tags("Tags di Prova da Eliminare")
+        
+        # Modifica tags
+        self.modifica_tags("Tags di Prova")
+
+        # Cancellazione tags
+        self.elimina_tags()
+
+        # Verifica tags
+        self.verifica_tags()
+
+    def creazione_tags(self, descrizione: str):
+        self.navigateTo("Tags")
+        self.wait_loader()  
+
+        self.find(By.CSS_SELECTOR, '#tabs > li:first-child .btn-primary > .fa-plus').click()
+        modal = self.wait_modal()
+
+        self.input(modal, 'Nome').setValue(descrizione)
+
+        modal.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+        self.wait_loader()
+
+    def modifica_tags(self, modifica:str):
+        wait = WebDriverWait(self.driver, 20)
+        self.navigateTo("Tags")
+        self.wait_loader()
+
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Nome"]/input'))).send_keys('Tags di Prova da Modificare', Keys.ENTER)
+        sleep(1)
+
+        self.find(By.XPATH, '//div[@id="tab_0"]//tbody//td[2]//div[1]').click()
+        self.wait_loader()
+        
+        self.input(None,'Nome').setValue(modifica)
+        self.find(By.XPATH, '//div[@id="tab_0"]//a[@id="save"]').click()
+        self.wait_loader()
+
+        self.navigateTo("Tags")
+        self.wait_loader()  
+
+        self.find(By.XPATH, '//th[@id="th_Nome"]/i[@class="deleteicon fa fa-times fa-2x"]').click()
+
+    def elimina_tags(self):
+        wait = WebDriverWait(self.driver, 20)
+        self.navigateTo("Tags")
+        self.wait_loader()  
+
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Nome"]/input'))).send_keys('Tags di Prova da Eliminare', Keys.ENTER)
+        sleep(1)
+
+        self.find(By.XPATH, '//div[@id="tab_0"]//tbody//td[2]//div[1]').click()
+        sleep(1)
+        
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_0"]//a[@class="btn btn-danger ask"]'))).click()
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//button[@class="swal2-confirm btn btn-lg btn-danger"]'))).click()
+        self.wait_loader()
+
+        self.find(By.XPATH, '//th[@id="th_Nome"]/i[@class="deleteicon fa fa-times fa-2x"]').click()
+
+    def verifica_tags(self):
+        wait = WebDriverWait(self.driver, 20)
+        self.navigateTo("Tags")
+        self.wait_loader()    
+
+        #verifica elemento modificato
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Nome"]/input'))).send_keys("Tags di Prova", Keys.ENTER)
+        sleep(1)
+
+        modificato=self.driver.find_element(By.XPATH,'//tbody//tr[1]//td[3]').text
+        self.assertEqual("Tags di Prova",modificato)
+        self.find(By.XPATH, '//i[@class="deleteicon fa fa-times fa-2x"]').click()
+
+        #verifica elemento eliminato
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Nome"]/input'))).send_keys("Tags di Prova da Eliminare", Keys.ENTER)
+        sleep(1)
+
+        eliminato=self.driver.find_element(By.XPATH,'//tbody//tr[1]//td[@class="dataTables_empty"]').text
+        self.assertEqual("La ricerca non ha portato alcun risultato.",eliminato)
