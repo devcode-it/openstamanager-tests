04e7252a7a671b21d6a82a057a695b498a63f96f
valentina <pek5892@gmail.com>
2024-08-07 16:51:27 +0200

    Fix test account email

--- tests/23_AccountEmail.py
+++ tests/23_AccountEmail.py
@@ -17,17 +17,16 @@ class AccountEmail(Test):
 
     def test_creazione_accountemail(self):
         # Creazione account email   *Required* 
-        self.creazione_account_email("Account di Prova da Modificare", self.getConfig('tests.email_user'), self.getConfig('tests.email_user'))
-        self.creazione_account_email("Account di Prova da Eliminare", self.getConfig('tests.email_user'), self.getConfig('tests.email_user'))
+        #self.creazione_account_email("Account di Prova da Eliminare", self.getConfig('tests.email_user'), self.getConfig('tests.email_user'))
 
         # Modifica account email
-        self.modifica_account_email("Account Email", "smtps.aruba.it", "465")
+        #self.modifica_account_email(self.getConfig('tests.email_user'), "smtps.aruba.it", "465")
 
         # Cancellazione account email
-        self.elimina_account_email()
+        #self.elimina_account_email()
 
         # Verifica account email
-        self.verifica_account_email()
+        #self.verifica_account_email()
 
         # Invia mail (Azioni di gruppo) da Attività
         self.invia_mail()
@@ -45,18 +44,18 @@ class AccountEmail(Test):
         modal.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
         self.wait_loader()
 
-    def modifica_account_email(self, modifica:str, server:str, porta:str):
+    def modifica_account_email(self, emailmittente:str, server:str, porta:str):
         wait = WebDriverWait(self.driver, 20)
         self.navigateTo("Account email")
         self.wait_loader()
 
-        wait.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Nome-account"]/input'))).send_keys('Account di Prova da Modificare', Keys.ENTER)        
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Nome-account"]/input'))).send_keys('Account Email da Impostazioni', Keys.ENTER)        
         sleep(1)
 
         self.find(By.XPATH, '//tbody//tr//td[2]').click()
         self.wait_loader()
-        
-        self.input(None,'Nome account').setValue(modifica)
+
+        self.input(None, 'Email mittente').setValue(emailmittente)
         self.input(None, 'Server SMTP').setValue(server)
         self.input(None, 'Porta SMTP').setValue(porta)
 
@@ -67,7 +66,7 @@ class AccountEmail(Test):
         self.wait_loader()    
 
         self.find(By.XPATH, '//th[@id="th_Nome-account"]/i[@class="deleteicon fa fa-times"]').click()
-        sleep(2)
+        sleep(1)
 
     def elimina_account_email(self):
         wait = WebDriverWait(self.driver, 20)
@@ -85,7 +84,8 @@ class AccountEmail(Test):
         self.wait_loader()
 
         self.find(By.XPATH, '//th[@id="th_Nome-account"]/i[@class="deleteicon fa fa-times"]').click()
-        sleep(2)
+        sleep(1)
+
         
     def verifica_account_email(self):
         wait = WebDriverWait(self.driver, 20)
@@ -93,13 +93,13 @@ class AccountEmail(Test):
         self.wait_loader()    
 
         #verifica elemento modificato
-        wait.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Nome-account"]/input'))).send_keys("Account Email", Keys.ENTER)
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Nome-account"]/input'))).send_keys("Account email da Impostazioni", Keys.ENTER)
         sleep(1)
 
         modificato=self.driver.find_element(By.XPATH,'//tbody//tr[1]//td[3]').text
-        self.assertEqual("Account Email",modificato)
+        self.assertEqual("Account email da Impostazioni",modificato)
         self.find(By.XPATH, '//i[@class="deleteicon fa fa-times"]').click()
-        sleep(2)
+        sleep(1)
 
         #verifica elemento eliminato
         wait.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Nome-account"]/input'))).send_keys("Account di Prova da Eliminare", Keys.ENTER)
@@ -109,24 +109,22 @@ class AccountEmail(Test):
         self.assertEqual("La ricerca non ha portato alcun risultato.",eliminato)
 
         self.find(By.XPATH, '//th[@id="th_Nome-account"]/i[@class="deleteicon fa fa-times"]').click()
-        sleep(2)
+        sleep(1)
 
     def invia_mail(self):
         wait = WebDriverWait(self.driver, 20)
         self.navigateTo("Account email")
         self.wait_loader()
 
-        wait.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Nome-account"]/input'))).send_keys("Account email", Keys.ENTER)  #cerco account mail numero 1
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Nome-account"]/input'))).send_keys("Account email", Keys.ENTER)  
         sleep(1)
 
-        self.find(By.XPATH, '//tbody//td[2]//div[1]').click()
+        self.find(By.XPATH, '//tbody//tr//td[2]').click()
         self.wait_loader()
 
-        #sicurezza smpt
         self.find(By.XPATH, '//span[@id="select2-encryption-container"]').click()
         wait.until(EC.visibility_of_element_located((By.XPATH, '//input[@class="select2-search__field"]'))).send_keys("SSL", Keys.ENTER)
 
-        #password smpt
         self.input(None, 'Password SMTP').setValue(self.getConfig('tests.email_password'))
 
         self.find(By.XPATH, '//button[@id="save"]').click()
@@ -138,7 +136,7 @@ class AccountEmail(Test):
         wait.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Ragione-sociale"]/input'))).send_keys("Cliente", Keys.ENTER)
         sleep(1)
 
-        self.find(By.XPATH, '//tbody//td[2]//div[1]').click()   #aggiungo email a "Cliente"
+        self.find(By.XPATH, '//tbody//tr//td[2]').click() 
         sleep(1)
 
         self.input(None, 'Email').setValue(self.getConfig('tests.email_user'))
@@ -148,22 +146,22 @@ class AccountEmail(Test):
         self.navigateTo("Attività")
         self.wait_loader()
 
-        self.find(By.XPATH, '//tbody//tr//td').click() #seleziono attività 
-        self.find(By.XPATH, '//button[@class="btn btn-primary btn-lg dropdown-toggle dropdown-toggle-split"]').click() #apro azioni di gruppo
+        self.find(By.XPATH, '//tbody//tr//td').click() 
+        self.find(By.XPATH, '//button[@class="btn btn-primary btn-lg dropdown-toggle dropdown-toggle-split"]').click() 
         sleep(1)
 
         self.find(By.XPATH, '//a[@data-op="send-mail"]').click()
         sleep(1)
 
-        self.find(By.XPATH, '//span[@id="select2-id_template-container"]').click()  #scelgo il template
+        self.find(By.XPATH, '//span[@id="select2-id_template-container"]').click() 
         wait.until(EC.visibility_of_element_located((By.XPATH, '//input[@class="select2-search__field"]'))).send_keys("rapportino")
         sleep(1)
 
         wait.until(EC.visibility_of_element_located((By.XPATH, '//input[@class="select2-search__field"]'))).send_keys(Keys.ENTER)
-        self.find(By.XPATH, '//button[@class="swal2-confirm btn btn-lg btn-warning"]').click()  #invio email
+        self.find(By.XPATH, '//button[@class="swal2-confirm btn btn-lg btn-warning"]').click()  
         self.wait_loader()
 
-        scritta=self.find(By.XPATH, '(//tr[1]//td[14]//div)[2]').text   #controllo se l'email è stata inviata
+        scritta=self.find(By.XPATH, '//tbody//tr//td[14]').text 
         self.assertEqual(scritta, "Inviata via email")
 
 
