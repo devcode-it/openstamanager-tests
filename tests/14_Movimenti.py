623a20c204885fc30e6ad367f029de6ef316b3cd
valentina <pek5892@gmail.com>
2024-08-20 16:00:28 +0200

    Fix

--- tests/14_Movimenti.py
+++ tests/14_Movimenti.py
@@ -98,15 +98,15 @@ class Movimenti(Test):
         wait.until(EC.visibility_of_element_located((By.XPATH, '//a[@id="link-tab_10"]'))).click()
 
         movimento = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr//td[2]'))).text
-        fattura_vendita = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[2]//td[2]'))).text
-        ddt_uscita = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[3]//td[2]'))).text
-        ddt_entrata = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[4]//td[2]'))).text
-        fattura_acquisto2 = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[5]//td[2]'))).text
-        fattura_acquisto = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[6]//td[2]'))).text
-        attività = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[7]//td[2]'))).text
-        eliminazioneserial = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[8]//td[2]'))).text
-        serial = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[9]//td[2]'))).text
-        carico = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[10]//td[2]'))).text
+        fattura_vendita = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[3]//td[2]'))).text
+        ddt_uscita = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[4]//td[2]'))).text
+        ddt_entrata = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[5]//td[2]'))).text
+        fattura_acquisto2 = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[6]//td[2]'))).text
+        fattura_acquisto = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[7]//td[2]'))).text
+        attività = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[8]//td[2]'))).text
+        eliminazioneserial = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[9]//td[2]'))).text
+        serial = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[10]//td[2]'))).text
+        carico = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="tab_10"]//tbody//tr[11]//td[2]'))).text
 
         self.assertEqual(movimento, "10,00")
         self.assertEqual(fattura_vendita, "-1,00")
