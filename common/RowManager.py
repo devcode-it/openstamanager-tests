ce498b7567097ac10ff8a0520dbab2fb917b212f
valentina <pek5892@gmail.com>
2025-07-28 17:18:17 +0200

    fix: rowmanager

--- common/RowManager.py
+++ common/RowManager.py
@@ -3,9 +3,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebElement
+from selenium.common.exceptions import NoSuchElementException
 from .Test import Test, get_text
-from .Input import Input, Select
-from .functions import get_cache_directory
+from .Input import Input, Select, Checkbox
+from .functions import get_cache_directory, wait_for_element_and_click, send_keys_and_click, wait_loader, wait_for_dropdown_and_select
 import json
 import os
 import glob
@@ -20,70 +21,96 @@ class RowManager:
         return self.tester.find(By.XPATH, xpath)
 
     def add_sconto(self, data: dict):
-        self.tester.find(By.XPATH, '//button[@class="btn btn-primary dropdown-toggle"]').click()
-        self.tester.find(By.XPATH, '//a[@data-title="Aggiungi sconto/maggiorazione"]').click()
+        wait_for_dropdown_and_select(
+            self.tester.driver,
+            self.wait,
+            '//button[@class="btn btn-primary dropdown-toggle"]',
+            option_xpath='//a[@data-title="Aggiungi sconto/maggiorazione"]'
+        )
         modal = self.tester.wait_modal()
 
         if 'descrizione' in data:
-            self.input(modal, 'Descrizione').setValue(data['descrizione'])
+            descrizione_input = self.input_exact(modal, 'Descrizione')
+            if descrizione_input:
+                descrizione_input.setValue(data['descrizione'])
+            else:
+                print(f"Errore: campo 'Descrizione' non trovato nel modal")
 
         if 'sconto_percentuale' in data:
-            self.input(
-                modal, 'Sconto/maggiorazione percentuale').setValue(data['sconto_percentuale'])
+            sconto_perc_input = self.input(modal, 'Sconto/maggiorazione percentuale')
+            if sconto_perc_input:
+                sconto_perc_input.setValue(data['sconto_percentuale'])
+            else:
+                print(f"Errore: campo 'Sconto/maggiorazione percentuale' non trovato nel modal")
         elif 'sconto_unitario' in data:
-            self.input(
-                modal, 'Sconto/maggiorazione unitario').setValue(data['sconto_unitario'])
+            sconto_unit_input = self.input(modal, 'Sconto/maggiorazione unitario')
+            if sconto_unit_input:
+                sconto_unit_input.setValue(data['sconto_unitario'])
+            else:
+                print(f"Errore: campo 'Sconto/maggiorazione unitario' non trovato nel modal")
 
-        submit_button = modal.find_element(By.CSS_SELECTOR, 'button[onclick="submitForm()"]')
-        submit_button.click()
+        wait_for_element_and_click(self.tester.driver, self.wait, 'button[onclick="submitForm()"]', By.CSS_SELECTOR)
         self.wait.until(EC.staleness_of(modal))
 
     def add_descrizione(self, data: dict):
-        self.get_button('Descrizione').click()
+        button = self.get_button('Descrizione')
+        button.click()
+        wait_loader(self.tester.driver, self.wait)
         modal = self.tester.wait_modal()
 
-        self.input(modal, 'Descrizione').setValue(data['descrizione'])
+        descrizione_input = self.input_exact(modal, 'Descrizione')
+        if descrizione_input:
+            descrizione_input.setValue(data['descrizione'])
+        else:
+            print(f"Errore: campo 'Descrizione' non trovato nel modal")
 
-        submit_button = modal.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
-        submit_button.click()
+        wait_for_element_and_click(self.tester.driver, self.wait, 'button[type="submit"]', By.CSS_SELECTOR)
         self.wait.until(EC.staleness_of(modal))
 
     def add_riga(self, data: dict):
-        self.tester.find(By.XPATH, '//a[@class="btn btn-primary"]').click()
+        wait_for_element_and_click(self.tester.driver, self.wait, '//a[@class="btn btn-primary"]')
         modal = self.tester.wait_modal()
 
         self.fill(modal, data)
 
-        submit_button = modal.find_element(By.CSS_SELECTOR, 'button[onclick="submitForm()"]')
-        submit_button.click()
+        wait_for_element_and_click(self.tester.driver, self.wait, 'button[onclick="submitForm()"]', By.CSS_SELECTOR)
         self.wait.until(EC.staleness_of(modal))
 
     def add_articolo(self, data: dict):
         article_code = data.get('codice', '')
 
-        self.tester.find(By.XPATH, '//span[@id="select2-id_articolo-container"]').click()
-        search_input = self.tester.find(By.XPATH, '//span[@class="select2-search select2-search--dropdown"]//input[@type="search"]')
-        search_input.send_keys(article_code, Keys.ENTER)
+        wait_for_dropdown_and_select(
+            self.tester.driver,
+            self.wait,
+            '//span[@id="select2-id_articolo-container"]',
+            option_text=article_code
+        )
 
-        self.wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@onclick="salvaArticolo()"]')))
-
-        save_button = self.tester.find(By.XPATH, '//button[@onclick="salvaArticolo()"]')
-        save_button.click()
-
-        self.wait.until(EC.invisibility_of_element_located((By.XPATH, '//button[@onclick="salvaArticolo()"]')))
+        wait_for_element_and_click(self.tester.driver, self.wait, '//button[@onclick="salvaArticolo()"]')
 
     def fill(self, modal, data: dict):
-        self.input(modal, 'Descrizione').setValue(data['descrizione'])
+        descrizione_input = self.input_exact(modal, 'Descrizione')
+        if descrizione_input:
+            descrizione_input.setValue(data['descrizione'])
+        else:
+            print(f"Errore: campo 'Descrizione' non trovato nel modal")
 
         if 'qta' in data:
-            self.input(modal, 'Q.tà').setValue(data['qta'])
+            qta_input = self.input(modal, 'Q.tà')
+            if qta_input:
+                qta_input.setValue(data['qta'])
+            else:
+                print(f"Errore: campo 'Q.tà' non trovato nel modal")
 
-        if (self.input(modal, 'Prezzo unitario di vendita')):
-            self.input(modal, 'Prezzo unitario di vendita').setValue(
-                data['prezzo_unitario'])
+        prezzo_vendita_input = self.input(modal, 'Prezzo unitario di vendita')
+        if prezzo_vendita_input:
+            prezzo_vendita_input.setValue(data['prezzo_unitario'])
         else:
-            self.input(modal, 'Prezzo unitario').setValue(
-                data['prezzo_unitario'])
+            prezzo_unitario_input = self.input(modal, 'Prezzo unitario')
+            if prezzo_unitario_input:
+                prezzo_unitario_input.setValue(data['prezzo_unitario'])
+            else:
+                print(f"Errore: nessun campo prezzo trovato nel modal")
 
         sconto_xpath = Input.xpath(modal, None, 'input[@id="sconto"]')
         sconto_input = self.tester.find(By.XPATH, sconto_xpath)
@@ -103,19 +130,55 @@ class RowManager:
             tipo_sconto.setValue(data['tipo_sconto'])
 
         if 'iva' in data:
-            select = self.input(modal, 'IVA')
-            select.setByText(data['iva'])
+            iva_select = self.input(modal, 'IVA')
+            if iva_select:
+                iva_select.setByText(data['iva'])
+            else:
+                print(f"Errore: campo 'IVA' non trovato nel modal")
 
         if 'rivalsa_inps' in data:
-            select = self.input(modal, 'Rivalsa INPS')
-            select.setByText(data['rivalsa_inps'])
+            rivalsa_select = self.input(modal, 'Rivalsa INPS')
+            if rivalsa_select:
+                rivalsa_select.setByText(data['rivalsa_inps'])
+            else:
+                print(f"Errore: campo 'Rivalsa INPS' non trovato nel modal")
 
         if 'ritenuta_acconto' in data:
-            select = self.input(modal, "Ritenuta d'acconto")
-            select.setByText(data['ritenuta_acconto'])
+            ritenuta_select = self.input(modal, "Ritenuta d'acconto")
+            if ritenuta_select:
+                ritenuta_select.setByText(data['ritenuta_acconto'])
+            else:
+                print(f"Errore: campo 'Ritenuta d'acconto' non trovato nel modal")
 
     def input(self, element: WebElement, name=None, css_id=None):
         return self.tester.input(element, name, css_id)
+
+    def input_exact(self, element: WebElement, name: str):
+        """Find input by exact label text match"""
+        element_id = element.get_attribute("id")
+
+        prefix = ''
+        if element_id:
+            prefix = '//*[@id="' + element_id + '"]'
+
+        # Use exact text match instead of contains
+        prefix += f'//label[text()="{name}" or normalize-space(text())="{name}"]/parent::div/parent::div//'
+
+        element_types = [
+            {'xpath': prefix + 'select', 'class': Select},
+            {'xpath': prefix + 'input[@type="checkbox"]', 'class': Checkbox},
+            {'xpath': prefix + 'input', 'class': Input},
+            {'xpath': prefix + 'textarea', 'class': Input}
+        ]
+
+        for elem_type in element_types:
+            try:
+                found_element = element.find_element(By.XPATH, elem_type['xpath'])
+                return elem_type['class'](self.tester.driver, found_element)
+            except NoSuchElementException:
+                continue
+
+        return None
 
     def compile(self, filename: str):
         importi = self.read(filename)
