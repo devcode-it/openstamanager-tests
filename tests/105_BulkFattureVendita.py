ec9140e0a5c7d17507142617ec3c5af47e9bc458
valentina <pek5892@gmail.com>
2026-06-26 17:20:50 +0200

    feat: test aggiorna banca, esporta, esporta stampe, esporta stampe fe, esporta ricevute, esporta xml, invia fatture

--- tests/105_BulkFattureVendita.py
+++ tests/105_BulkFattureVendita.py
@@ -2,7 +2,7 @@ from common.Test import Test
 from common.RowManager import RowManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-
+import os
 
 class FattureVendita(Test):
     def setUp(self):
@@ -10,65 +10,97 @@ class FattureVendita(Test):
         self.expandSidebar("Vendite")
 
     def test_bulk_fattura_vendita(self):
-        #TODO: Aggiorna banca
-        #self.aggiorna_banca()
+        self.aggiorna_banca()
         self.cambia_sezionale()
         self.controlla_fatture_elettroniche()
         self.duplica_selezionati()
         self.elimina_selezionati()
         self.emetti_fatture()
-        #TODO: Esporta
-        #self.esporta_selezionati()
-        #TODO: Esporta stampe
-        #self.esporta_stampe()
-        #TODO: Esporta stampe FE
-        #self.esporta_stampe_fe()
-        #TODO: Esporta ricevute
+        self.esporta_selezionati()
+        self.esporta_stampe()
+        self.esporta_stampe_fe()
         #self.esporta_ricevute()
-        #TODO: Esporta XML
-        #self.esporta_xml()
+        self.esporta_xml()
         self.genera_fatture_elettroniche()
-        #TODO: Invia fatture
-        #self.invia_fatture()
+        self.invia_fatture()
         self.registrazione_contabile()
+
+    def aggiorna_banca(self):
+        self.expandSidebar("Strumenti")
+        self.expandSidebar("Tabelle")
+        self.navigate_to_and_wait("Banche")
+
+        self.click_add_button()
+        modal = self.wait_modal()
+        
+        self.wait_for_dropdown_and_select('//span[@id="select2-id_anagrafica-container"]', option_text='Admin spa')
+        self.input(modal, 'Nome').setValue('Test')
+        self.input(modal, 'IBAN').setValue('IT60X0542811101000000123456')
+        self.input(modal, 'BIC').setValue('BNPAITMM')
+        self.wait_for_element_and_click('button[type="submit"]', By.CSS_SELECTOR)
+
+        self.expandSidebar("Vendite")
+        self.navigate_to_and_wait("Fatture di vendita")
+        self.search_by_th("th_Ragione-sociale", "Cliente")
+        self.wait_for_search_results()
+
+        self.wait_for_element_and_click('//tbody//tr//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="change_bank"]'
+        )
+
+        self.wait_for_dropdown_and_select(
+            '//span[@id="select2-id_banca-container"]',
+            option_text="Test"
+        )
+
+        self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
 
     def cambia_sezionale(self):
         self.navigate_to_and_wait("Fatture di vendita")
 
-        self.wait_for_element_and_click('//tbody//tr//td')
-        self.wait_for_element_and_click('//button[@data-toggle="dropdown"]')
-        self.wait_for_element_and_click('//a[@data-op="change_segment"]')
+        self.wait_for_element_and_click('//tbody//tr[3]//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="change_segment"]'
+        )
 
         self.wait_for_dropdown_and_select(
             '//span[@id="select2-id_segment-container"]',
             option_text='Autofatture'
         )
         self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
+
         self.wait_for_dropdown_and_select(
             '//span[@id="select2-id_segment_-container"]',
             option_text='Autofatture'
         )
 
-        self.wait_for_element_and_click('//tbody//tr//td')
-        self.wait_for_element_and_click('//button[@data-toggle="dropdown"]')
-        self.wait_for_element_and_click('//a[@data-op="change_segment"]')
-
+        self.wait_for_element_and_click('//tbody//tr[3]//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="change_segment"]'
+        )
         self.wait_for_dropdown_and_select(
             '//span[@id="select2-id_segment-container"]',
-            option_text='Standard vendite'
+            option_text='Vendite'
         )
         self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
         self.wait_for_dropdown_and_select(
             '//span[@id="select2-id_segment_-container"]',
-            option_text='Standard vendite'
+            option_text='Vendite'
         )
     
     def controlla_fatture_elettroniche(self):
         self.navigate_to_and_wait("Fatture di vendita")
 
         self.wait_for_element_and_click('//tbody//tr//td')
-        self.wait_for_element_and_click('//button[@data-toggle="dropdown"]')
-        self.wait_for_element_and_click('//a[@data-op="check_bulk"]')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="check_bulk"]'
+        )
+
         self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
 
         self.driver.switch_to.window(self.driver.window_handles[1])
@@ -80,40 +112,160 @@ class FattureVendita(Test):
         self.navigate_to_and_wait("Fatture di vendita")
 
         self.wait_for_element_and_click('//tbody//tr//td')
-        self.wait_for_element_and_click('//button[@data-toggle="dropdown"]')
-        self.wait_for_element_and_click('//a[@data-op="copy_bulk"]')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="copy_bulk"]'
+        )
         self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
         self.wait_for_element_and_click('//tbody//tr//td')
 
     def elimina_selezionati(self):
         self.navigate_to_and_wait("Fatture di vendita")
+        self.search_by_th("th_Numero", "0002/2026")
 
         self.wait_for_element_and_click('//tbody//tr//td')
-        self.wait_for_element_and_click('//button[@data-toggle="dropdown"]')
-        self.wait_for_element_and_click('//a[@data-op="delete_bulk"]')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="delete_bulk"]'
+        )
         self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
 
-        test = self.wait_driver.until(EC.visibility_of_element_located((By.XPATH, '//tbody//tr//td[2]'))).text
-        self.assertEqual(test, "0002/2026")
+        eliminato = self.get_empty_table_message()
+        self.assertEqual("Nessun dato presente nella tabella", eliminato)
+        self.clear_filters()
 
     def emetti_fatture(self):
         self.navigate_to_and_wait("Fatture di vendita")
 
         self.wait_for_element_and_click('//tbody//tr//td')
-        self.wait_for_element_and_click('//button[@data-toggle="dropdown"]')
-        self.wait_for_element_and_click('//a[@data-op="change_status"]')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="change_status"]'
+        )
         self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
 
-        stato = self.wait_driver.until(EC.visibility_of_element_located((By.XPATH, '//tbody//tr//td[11]'))).text
+        stato = self.get_table_text(1, 11)
         self.assertEqual(stato, "Emessa")
         self.wait_for_element_and_click('//tbody//tr//td')
+
+    def esporta_selezionati(self):
+        self.navigate_to_and_wait("Fatture di vendita")
+
+        self.wait_for_element_and_click('//tbody//tr//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="export_csv"]'
+        )
+        self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
+
+        download_dir = os.path.expanduser('~/Scaricati')
+        files_before = os.listdir(download_dir) if os.path.exists(download_dir) else []
+        
+        import time
+        time.sleep(2)
+        
+        files_after = os.listdir(download_dir) if os.path.exists(download_dir) else []
+        new_files = set(files_after) - set(files_before)
+        
+        csv_files = [f for f in new_files if f.endswith('.csv')]
+        self.assertTrue(len(csv_files) > 0, "Nessun file CSV scaricato")
+
+    def esporta_stampe(self):
+        self.navigate_to_and_wait("Fatture di vendita")
+
+        self.wait_for_element_and_click('//tbody//tr//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="export_bulk"]'
+        )
+        self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
+
+        download_dir = os.path.expanduser('~/Scaricati')
+        files_before = os.listdir(download_dir) if os.path.exists(download_dir) else []
+        
+        import time
+        time.sleep(2)
+        
+        files_after = os.listdir(download_dir) if os.path.exists(download_dir) else []
+        new_files = set(files_after) - set(files_before)
+        
+        csv_files = [f for f in new_files if f.endswith('.zip')]
+        self.assertTrue(len(csv_files) > 0, "Nessun file CSV scaricato")
+
+    def esporta_stampe_fe(self):
+        self.navigate_to_and_wait("Fatture di vendita")
+
+        self.wait_for_element_and_click('//tbody//tr//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="export_fe_bulk"]'
+        )
+        self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
+
+        download_dir = os.path.expanduser('~/Scaricati')
+        files_before = os.listdir(download_dir) if os.path.exists(download_dir) else []
+        
+        import time
+        time.sleep(2)
+        
+        files_after = os.listdir(download_dir) if os.path.exists(download_dir) else []
+        new_files = set(files_after) - set(files_before)
+        
+        csv_files = [f for f in new_files if f.endswith('.zip')]
+        self.assertTrue(len(csv_files) > 0, "Nessun file ZIP scaricato")
+
+    def esporta_ricevute(self):
+        self.navigate_to_and_wait("Fatture di vendita")
+
+        self.wait_for_element_and_click('//tbody//tr//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="export_receipts_bulk"]'
+        )
+        self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
+
+        download_dir = os.path.expanduser('~/Scaricati')
+        files_before = os.listdir(download_dir) if os.path.exists(download_dir) else []
+        
+        import time
+        time.sleep(2)
+        
+        files_after = os.listdir(download_dir) if os.path.exists(download_dir) else []
+        new_files = set(files_after) - set(files_before)
+        
+        csv_files = [f for f in new_files if f.endswith('.zip')]
+        self.assertTrue(len(csv_files) > 0, "Nessun file ZIP scaricato")
+
+    def esporta_xml(self):
+        self.navigate_to_and_wait("Fatture di vendita")
+
+        self.wait_for_element_and_click('//tbody//tr//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="export_xml_bulk"]'
+        )
+        self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
+
+        download_dir = os.path.expanduser('~/Scaricati')
+        files_before = os.listdir(download_dir) if os.path.exists(download_dir) else []
+        
+        import time
+        time.sleep(2)
+        
+        files_after = os.listdir(download_dir) if os.path.exists(download_dir) else []
+        new_files = set(files_after) - set(files_before)
+        
+        csv_files = [f for f in new_files if f.endswith('.zip')]
+        self.assertTrue(len(csv_files) > 0, "Nessun file ZIP scaricato")
 
     def genera_fatture_elettroniche(self):
         self.navigate_to_and_wait("Fatture di vendita")
 
         self.wait_for_element_and_click('//tbody//tr//td')
-        self.wait_for_element_and_click('//button[@data-toggle="dropdown"]')
-        self.wait_for_element_and_click('//a[@data-op="generate_xml"]')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="generate_xml"]'
+        )
         self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
 
         self.driver.switch_to.window(self.driver.window_handles[1])
@@ -128,18 +280,29 @@ class FattureVendita(Test):
         self.navigate_to_and_wait("Fatture di vendita")
         self.wait_for_element_and_click('//tbody//tr//td')
 
-    def registrazione_contabile(self):
+    def invia_fatture(self):
         self.navigate_to_and_wait("Fatture di vendita")
 
-        search_input = self.wait_driver.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Numero"]/input')))
-        self.send_keys_and_wait(search_input, "0001", wait_modal=False)
+        self.wait_for_element_and_click('//tbody//tr//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="send-invoices"]'
+        )
+        self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
+        self.clear_filters()
+
+    def registrazione_contabile(self):
+        self.navigate_to_and_wait("Fatture di vendita")
+        self.search_by_th("th_Numero", "0001")
 
         self.wait_for_element_and_click('//tbody//tr//td')
-        self.wait_for_element_and_click('//button[@data-toggle="dropdown"]')
-        self.wait_for_element_and_click('//a[@data-op="registrazione_contabile"]')
-
-        totale = self.wait_driver.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="totale_dare_add"]'))).text
-        self.assertEqual(totale, "305,98 €")
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="registrazione_contabile"]'
+        )
+        modal = self.wait_modal()
+        totale = self.find(By.XPATH, '//th[@id="totale_dare_add"]').text
+        self.assertEqual(totale, "323,06")
 
         self.wait_for_element_and_click('//button[@type="submit"]')
 
