340e2050ea300de62384ed805099ce2eec57e370
valentina <pek5892@gmail.com>
2026-06-26 15:19:16 +0200

    feat: test cambia metodo di pagamento e duplica contratti

--- tests/102_BulkContratti.py
+++ tests/102_BulkContratti.py
@@ -11,52 +11,77 @@ class Contratti(Test):
 
     def test_bulk_contratto(self):
         self.cambia_stato()
-        #TODO: cambia metodo di pagamento
-        #self.cambia_metodo_pagamento()
+        self.cambia_metodo_pagamento()
         self.fattura_contratti()
         self.rinnova_contratti()
-        #TODO: duplica contratti
+        self.duplica_contratti()
     
     def cambia_stato(self):
         self.navigate_to_and_wait("Contratti")
 
-        self.send_keys_and_wait(self.wait_driver.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Numero"]/input'))), "2", wait_modal=False)
+        self.search_by_th("th_Numero", "1", wait_modal=False)
+
 
         self.wait_for_element_and_click('//tbody//tr//td')
-        self.wait_for_element_and_click('//button[@data-toggle="dropdown"]')
-        self.wait_for_element_and_click('//a[@data-op="change_status"]')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="change_status"]'
+        )
 
         self.select_state('In lavorazione')
         self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
 
-        stato = self.wait_driver.until(
-            EC.visibility_of_element_located((By.XPATH, '//tbody//tr//td[5]'))
-        ).text
+        stato = self.get_table_text(1, 5)
         self.assertEqual(stato, "In lavorazione")
 
+        self.wait_for_element_and_click('//tbody//tr//td')
+        self.clear_filters()
+
+    def cambia_metodo_pagamento(self):
+        self.navigate_to_and_wait("Contratti")
+
+        self.search_by_th("th_Numero", "1", wait_modal=False)
+
+        self.wait_for_element_and_click('//tbody//tr//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="change_payment"]'
+        )
+
+        self.wait_for_dropdown_and_select(
+            '//span[@id="select2-id_pagamento-container"]',
+            option_text='Bonifico 120gg d.f.'
+        )
+        self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
+
+        self.click_first_result()
+        metodo_pagamento = self.wait_for_element_and_click('//span[@id="select2-id_pagamento-container"]').text
+        self.assertEqual(metodo_pagamento, "MP05 - Bonifico 120gg d.f.")
+
+        self.navigate_to_and_wait("Contratti")
         self.wait_for_element_and_click('//tbody//tr//td')
         self.clear_filters()
 
     def fattura_contratti(self):
         self.navigate_to_and_wait("Contratti")
 
-        self.send_keys_and_wait(self.wait_driver.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Numero"]/input'))), "2", wait_modal=False)
+        self.search_by_th("th_Numero", "1", wait_modal=False)
 
         self.wait_for_element_and_click('//tbody//tr//td')
-        self.wait_for_element_and_click('//button[@data-toggle="dropdown"]')
-        self.wait_for_element_and_click('//a[@data-op="create_invoice"]')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="create_invoice"]'
+        )
 
         self.wait_for_dropdown_and_select('//span[@id="select2-raggruppamento-container"]', option_text='Cliente')
         self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
 
         self.navigate_to_and_wait("Fatture di vendita")
 
-        tipo = self.wait_driver.until(
-            EC.visibility_of_element_located((By.XPATH, '//tbody//tr[1]//td[5]'))
-        ).text
+        tipo = self.get_table_text(3, 5)
         self.assertEqual(tipo, "Fattura immediata di vendita")
 
-        self.wait_and_click_table_row(2, 4)
+        self.wait_and_click_table_row(3, 4)
         self.delete_current_and_clear()
 
         self.navigate_to_and_wait("Contratti")
@@ -65,26 +90,46 @@ class Contratti(Test):
     def rinnova_contratti(self):
         self.navigate_to_and_wait("Contratti")
 
-        self.send_keys_and_wait(self.wait_driver.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Numero"]/input'))), "1", wait_modal=False)
+        self.search_by_th("th_Numero", "1", wait_modal=False)
 
         self.click_first_table_row()
 
-        self.wait_driver.until(EC.visibility_of_element_located((By.XPATH, '//input[@id="data_accettazione"]'))).send_keys("01/01/2026")
-        self.wait_driver.until(EC.visibility_of_element_located((By.XPATH, '//input[@id="data_conclusione"]'))).send_keys("31/12/2026")
-        self.wait_for_dropdown_and_select('//span[@id="select2-idstato-container"]', option_text='Accettato')
+        self.send_keys_and_wait(self.driver.find_element(By.XPATH, '//input[@id="data_accettazione"]'), '01/01/2026')
+        self.send_keys_and_wait(self.driver.find_element(By.XPATH, '//input[@id="data_conclusione"]'), '31/12/2026')
+        self.wait_for_element_and_click('//div[@class="btn-group checkbox-buttons"]//label[@for="rinnovabile"]')
+        self.wait_for_dropdown_and_select('//span[@id="select2-id_stato-container"]', option_text='Accettato')
         self.wait_for_element_and_click('//button[@id="save"]')
 
         self.navigate_to_and_wait("Contratti")
 
         self.wait_for_element_and_click('//tbody//tr//td')
-        self.wait_for_element_and_click('//button[@data-toggle="dropdown"]')
-        self.wait_for_element_and_click('//a[@data-op="renew_contract"]')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="renew_contract"]'
+        )
+
         self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
 
-        self.wait_for_element_and_click('(//i[@class="deleteicon fa fa-times"])[1]')
+        self.clear_filters()
 
-        self.send_keys_and_wait(self.wait_driver.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Numero"]/input'))), "3", wait_modal=False)
+        self.send_keys_and_wait(self.driver.find_element(By.XPATH, '//th[@id="th_Numero"]/input'), "1", wait_modal=False)
 
-        self.wait_driver.until(EC.visibility_of_element_located((By.XPATH, '//tbody//tr//td[2]')))
+        self.wait_for_element_and_click('//tbody//tr//td[2]')
         self.click_first_table_row()
         self.delete_current_and_clear()
+
+    def duplica_contratti(self):
+        self.navigate_to_and_wait("Contratti")
+
+        self.search_by_th("th_Numero", "1", wait_modal=False)
+
+        self.wait_for_element_and_click('//tbody//tr//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="copy_bulk"]'
+        )
+
+        self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
+
+        self.clear_filters()
+        self.send_keys_and_wait(self.driver.find_element(By.XPATH, '//th[@id="th_Numero"]/input'), "2", wait_modal=False)
