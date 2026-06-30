e081619c23d047ae0baa2c29aa030010d5c58093
valentina <pek5892@gmail.com>
2026-06-30 12:40:56 +0200

    feat: test duplica ordine e invia mail

--- tests/106_BulkOrdiniFornitore.py
+++ tests/106_BulkOrdiniFornitore.py
@@ -10,24 +10,57 @@ class OrdiniFornitore(Test):
 
     def test_bulk_ordine_fornitore(self):
         self.cambia_stato()
-        #TODO: duplica
-        #TODO: invia mail
+        self.duplica()
+        self.invia_mail()
         
     def cambia_stato(self):
         self.navigate_to_and_wait("Ordini fornitore")
 
-        self.send_keys_and_wait(self.wait_driver.until(EC.visibility_of_element_located((By.XPATH, '//th[@id="th_Numero"]/input'))), "1", wait_modal=False)
+        self.search_by_th("th_Numero", "1")
 
         self.wait_for_element_and_click('//tbody//tr//td')
-        self.wait_for_element_and_click('//button[@data-toggle="dropdown"]')
-        self.wait_for_element_and_click('//a[@data-op="change_status"]')
-
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="change_status"]'
+        )
         self.select_state('Evaso')
         self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
 
-        stato = self.wait_driver.until(
-            EC.visibility_of_element_located((By.XPATH, '//tbody//tr//td[6]'))
-        ).text
+        stato = self.get_table_text(1, 6)
         self.assertEqual(stato, "Evaso")
+        self.clear_filters()
 
-        self.wait_for_element_and_click('//th[@id="th_Numero"]/i[@class="deleteicon fa fa-times"]')
+    def duplica(self):
+        self.navigate_to_and_wait("Ordini fornitore")
+
+        self.search_by_th("th_Numero", "1")
+
+        self.wait_for_element_and_click('//tbody//tr//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="copy_bulk"]'
+        )
+        self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
+
+        self.clear_filters()
+
+        numero = self.get_table_text(2, 2)
+        self.assertEqual(numero, "2")
+
+
+    def invia_mail(self):
+        self.navigate_to_and_wait("Ordini fornitore")
+
+        self.search_by_th("th_Numero", "1")
+
+        self.wait_for_element_and_click('//tbody//tr//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="send_mail"]'
+        )
+        self.wait_for_dropdown_and_select(
+            '//span[@id="select2-id_template-container"]',
+            option_text='Ordine'
+        )
+        self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
+        self.clear_filters()
