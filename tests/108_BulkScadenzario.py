c033b7037f921033a26aecaec29c3f87589a6806
valentina <pek5892@gmail.com>
2026-06-30 15:01:15 +0200

    feat: test aggiorna banca, info distinta, invia mail sollecito e registrazione contabile

--- tests/108_BulkScadenzario.py
+++ tests/108_BulkScadenzario.py
@@ -5,14 +5,81 @@ from selenium.webdriver.support import expected_conditions as EC
 class Scadenzario(Test):
     def setUp(self):
         super().setUp()
-        self.expandSidebar("Acquisti")
+        self.expandSidebar("Contabilità")
 
     def test_bulk_scadenzario(self):
-        #TODO: aggiorna banca
-        #self.aggiorna_banca()
-        #TODO: info distinta
-        #self.info_distinta()
-        #TODO: invia mail sollecito
-        #self.invia_mail_sollecito()
-        #TODO: registrazione contabile
+        self.aggiorna_banca()
+        self.info_distinta()
+        self.invia_mail_sollecito()
         self.registrazione_contabile()
+
+
+    def aggiorna_banca(self):
+        self.navigate_to_and_wait("Scadenzario")
+        self.search_by_th("th_Rif-Fattura", "0002/2026")
+        self.wait_for_search_results()
+
+        self.wait_for_element_and_click('//tbody//tr//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="change_bank"]'
+        )
+
+        self.wait_for_dropdown_and_select(
+            '//span[@id="select2-id_banca-container"]',
+            option_text="Test"
+        )
+
+        self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
+        self.clear_filters()
+
+    def info_distinta(self):
+        self.navigate_to_and_wait("Scadenzario")
+        self.search_by_th("th_Rif-Fattura", "0002/2026")
+        self.wait_for_search_results()
+
+        self.wait_for_element_and_click('//tbody//tr//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="change_distinta"]'
+        )
+
+        self.input(name='distinta').setValue('Test')
+
+        self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
+        self.clear_filters()
+
+    def invia_mail_sollecito(self):
+        self.navigate_to_and_wait("Scadenzario")
+        self.search_by_th("th_Rif-Fattura", "0002/2026")
+
+        self.wait_for_element_and_click('//tbody//tr//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="send_reminder"]'
+        )
+        self.wait_for_element_and_click('//button[@class="swal2-confirm btn btn-lg btn-success"]')
+        self.clear_filters()
+    
+    def registrazione_contabile(self):
+        self.navigate_to_and_wait("Scadenzario")
+        self.search_by_th("th_Rif-Fattura", "0002/2026")
+
+        self.wait_for_element_and_click('//tbody//tr//td')
+        self.wait_for_dropdown_and_select(
+            '//button[@data-toggle="dropdown"]',
+            option_xpath='//a[@data-op="registrazione-contabile"]'
+        )
+        modal = self.wait_modal()
+
+        self.wait_for_dropdown_and_select(
+            '//span[@id="select2-conto_add_1-container"]',
+            option_text='Banca C/C'
+        )
+
+        self.wait_for_element_and_click('//button[@type="submit"]')
+
+        self.expandSidebar("Vendite")
+        self.navigate_to_and_wait("Fatture di vendita")
+        self.clear_filters()
+
